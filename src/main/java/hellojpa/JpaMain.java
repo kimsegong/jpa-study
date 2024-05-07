403f4b4864ee4ca2cd89e576b913ab10203d033e
unknown <kimsegong@naver.com>
2024-05-07 15:36:14 +0900

    JpaChapter3

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -17,12 +17,12 @@ public class JpaMain {
 
         try{
             // code(데이터베이스 코드 작성하는곳)
-           Member member = em.find(Member.class, 150L);
-           member.setName("ZZZZZZ");
+           Member member = new Member(200L, "member200");
+          em.persist(member);
+          em.flush();
 
-            System.out.println("+++++++++++++++++++++++++++++++");
-
-            tx.commit();
+          System.out.println("===================");
+          tx.commit();
         } catch (Exception e){
             tx.rollback();
 
